40a1a2fd7a32953d4fc3d413d5fdffd9117df1d7
lly8666 <30583594+lly8666@users.noreply.github.com>
2026-08-25 17:13:40 +0800

    test native FakeTCP SACK wire format

--- internal/faketcp/packet_test.go
+++ internal/faketcp/packet_test.go
@@ -30,3 +30,19 @@ func TestSYNOptionsParsePayloadOffset(t *testing.T) {
 	if seg.Flags != FlagSYN || len(seg.Payload) != 0 { t.Fatalf("bad SYN %#v", seg) }
 	if pkt[20+12]>>4 != 7 { t.Fatalf("expected 28-byte TCP header, doff=%d", pkt[32]>>4) }
 }
+
+func TestSACKRoundTrip(t *testing.T) {
+	src, _ := IPv4(net.ParseIP("198.51.100.1"))
+	dst, _ := IPv4(net.ParseIP("198.51.100.2"))
+	want := []SACKBlock{{Start:1200, End:1400}, {Start:1600, End:1800}}
+	buf := make([]byte, 256)
+	pkt := MarshalIPv4TCPSACKInto(buf, src, dst, 2222, 3333, 900, 1000, FlagACK, 65535, want, nil, 9)
+	seg, err := ParseIPv4TCP(pkt)
+	if err != nil { t.Fatal(err) }
+	if seg.SACKN != len(want) { t.Fatalf("SACKN=%d", seg.SACKN) }
+	for i := range want {
+		if seg.SACK[i] != want[i] { t.Fatalf("sack[%d]=%#v want %#v", i, seg.SACK[i], want[i]) }
+	}
+	if checksum(pkt[:20]) != 0 { t.Fatal("invalid IPv4 checksum with SACK") }
+	if tcpChecksum(src, dst, pkt[20:]) != 0 { t.Fatal("invalid TCP checksum with SACK") }
+}
