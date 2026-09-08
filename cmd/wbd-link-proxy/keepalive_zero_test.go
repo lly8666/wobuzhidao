051817312d1993995e4115a171cddedbecedf0d1
lly8666 <30583594+lly8666@users.noreply.github.com>
2026-09-08 13:59:45 +0800

    test(link): lock keepalive zero runtime behavior [publish-test]

--- cmd/wbd-link-proxy/keepalive_zero_test.go
+++ cmd/wbd-link-proxy/keepalive_zero_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"os"
 	"testing"
 	"time"
+
+	"github.com/lly8666/wobuzhidao/internal/control"
+	"github.com/lly8666/wobuzhidao/internal/linkdata"
 )
 
 func TestClientKeepaliveDueDisabledAtZero(t *testing.T) {
@@ -29,5 +33,45 @@ func TestKeepaliveZeroNeverForcesRemoteRXExpiry(t *testing.T) {
 	last := time.Unix(1000, 0)
 	if clientRemoteRXExpired(last, last.Add(24*time.Hour), 0) {
 		t.Fatal("keepalive=0 must not force blackhole retirement")
+	}
+	if clientRemoteRXExpired(last, last.Add(24*time.Hour), -time.Second) {
+		t.Fatal("negative keepalive must remain disabled")
+	}
+}
+
+func TestClientDataLoopKeepaliveZeroDoesNotRetireSilentPeer(t *testing.T) {
+	client := udp4(t)
+	dtls := udp4(t)
+	defer client.Close()
+	defer dtls.Close()
+
+	path, err := linkdata.New(control.LinkConfig{
+		FECMode: control.FECOff, Scheduler: control.FECSchedulerNone,
+		MTU: 1400, LaneCount: 1,
+	}, maxBlocks)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stop := make(chan os.Signal, 1)
+	done := make(chan error, 1)
+	go func() {
+		done <- clientDataLoop(client, addr(dtls), path, establishedStartup{}, 0, stop)
+	}()
+
+	select {
+	case err := <-done:
+		t.Fatalf("keepalive=0 retired silent peer unexpectedly: %v", err)
+	case <-time.After(80 * time.Millisecond):
+	}
+
+	stop <- os.Interrupt
+	select {
+	case err := <-done:
+		if err != nil {
+			t.Fatalf("shutdown error=%v", err)
+		}
+	case <-time.After(time.Second):
+		t.Fatal("clientDataLoop did not stop with keepalive disabled")
 	}
 }
