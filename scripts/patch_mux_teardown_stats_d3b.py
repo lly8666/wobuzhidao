4789f720783e4ae9507bda963f7f4df35e352a04
lly8666 <30583594+lly8666@users.noreply.github.com>
2026-09-12 08:59:27 +0800

    test: expose late receiver delivery counters

--- scripts/patch_mux_teardown_stats_d3b.py
+++ scripts/patch_mux_teardown_stats_d3b.py
@@ -15,7 +15,7 @@ new = '''\tif worker != nil {
 \t}
 \tss := sess.assoc.SenderStats()
 \trs := sess.assoc.ReceiverStats()
-\tfmt.Printf("WBD_FAKETCP_MUX_STATS client=%d server=%d sender_enqueued=%d sender_acked=%d sender_sacked=%d sender_retired_sacked=%d sender_fast_retx=%d sender_rto_retx=%d sender_peak_pending=%d receiver_delivered=%d receiver_duplicates=%d receiver_out_of_order=%d receiver_forgiven_gaps=%d receiver_forgiven_bytes=%d receiver_peak_buffered_oo=%d\\n", flow.ClientPort, flow.ServerPort, ss.Enqueued, ss.Acked, ss.SACKed, ss.RetiredSACKed, ss.FastRetransmits, ss.RTOTransmits, ss.PeakPending, rs.Delivered, rs.Duplicates, rs.OutOfOrder, rs.ForgivenGaps, rs.ForgivenBytes, rs.PeakBufferedOO)
+\tfmt.Printf("WBD_FAKETCP_MUX_STATS client=%d server=%d sender_enqueued=%d sender_acked=%d sender_sacked=%d sender_retired_sacked=%d sender_fast_retx=%d sender_rto_retx=%d sender_peak_pending=%d receiver_delivered=%d receiver_duplicates=%d receiver_out_of_order=%d receiver_late_below_ack=%d receiver_below_next_drops=%d receiver_buffered_duplicates=%d receiver_forgiven_gaps=%d receiver_forgiven_bytes=%d receiver_peak_buffered_oo=%d\\n", flow.ClientPort, flow.ServerPort, ss.Enqueued, ss.Acked, ss.SACKed, ss.RetiredSACKed, ss.FastRetransmits, ss.RTOTransmits, ss.PeakPending, rs.Delivered, rs.Duplicates, rs.OutOfOrder, rs.LateBelowACK, rs.BelowNextDrops, rs.BufferedDuplicates, rs.ForgivenGaps, rs.ForgivenBytes, rs.PeakBufferedOO)
 \ts.table.Remove(flow)
 '''
 if s.count(old) != 1:
