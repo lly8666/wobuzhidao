27f662c1d134c7fe87b77ebecf364d7903dee11e
lly8666 <30583594+lly8666@users.noreply.github.com>
2026-09-18 21:52:16 +0800

    feat(tun): add userspace direct forwarding bridge

---
+++ internal/tunsplit/direct_other.go
@@ -0,0 +1,21 @@
+//go:build !windows
+
+package tunsplit
+
+import (
+	"errors"
+
+	"github.com/lly8666/wobuzhidao/internal/tunnel"
+)
+
+type DirectEngine struct{}
+
+func NewDirectEngine(interfaceIndex uint32, mtu int, output tunnel.Endpoint) (*DirectEngine, error) {
+	return nil, errors.New("userspace direct split is Windows-only")
+}
+
+func (e *DirectEngine) Inject(packet []byte) error {
+	return errors.New("userspace direct split is Windows-only")
+}
+
+func (e *DirectEngine) Close() error { return nil }
